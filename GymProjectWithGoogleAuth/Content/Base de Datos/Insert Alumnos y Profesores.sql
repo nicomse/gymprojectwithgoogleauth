7957bac07517404fd88725d470f48e57da8fffaf
Tomás Vainstein <tomasvainstein@gmail.com>
2019-06-27 23:14:47 -0300

    Se sube la BD y los Insert

---
+++ GymProjectWithGoogleAuth/Content/Base de Datos/Insert Alumnos y Profesores.sql
@@ -0,0 +1,263 @@
+USE [Taller6]
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Morgan', 'Freeman', 'morgan.freeman@gmail.com', '1170810538'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Johnny', 'Deep', 'johnny.deep@gmail.com', '1160548582'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Al', 'Pacino', 'al.pacino@gmail.com', '1150042473'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Robert', 'DeNiro', 'robert.deniro@gmail.com', '1145960050'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Denzel', 'Washington', 'denzel.washington@gmail.com', '1189930009'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Brad', 'Pitt', 'brad.pitt@gmail.com', '1159200265'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Bruce', 'Lee', 'bruce.lee@gmail.com', '1129887183'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Angelina', 'Jolie', 'angelina.jolie@gmail.com', '1170992449'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Leonardo', 'DiCaprio', 'leonardo.dicaprio@gmail.com', '1165570252'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Tom', 'Cruise', 'tom.cruise@gmail.com', '1131507294'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'John', 'Travolta', 'john.travolta@gmail.com', '1134386487'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Arnold', 'Schwarzenegger', 'arnold.schwarzenegger@gmail.com', '1160217987'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Sylvester', 'Stallone', 'sylvester.stallone@gmail.com', '1170869700'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Christian', 'Bale', 'christian.bale@gmail.com', '1170085187'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Keanu', 'Reeves', 'keanu.reeves@gmail.com', '1132155732'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Nicolas', 'Cage', 'nicolas.cage@gmail.com', '1150160998'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Hugh', 'Jackman', 'hugh.jackman@gmail.com', '1111287923'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Edward', 'Norton', 'edward.norton@gmail.com', '1120775567'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Bruce', 'Willis', 'bruce.willis@gmail.com', '1129110092'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaAlumno]'Tom', 'Hanks', 'tom.hanks@gmail.com', '1138077619'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaProfesor]'Will', 'Smith', 'will.smith@gmail.com', '1193396555'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaProfesor]'Vin', 'Diesel', 'vin.diesel@gmail.com', '1115582015'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaProfesor]'Matt', 'Damon', 'matt.damon@gmail.com', '1125307492'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaProfesor]'Tom', 'Hardy', 'tom.hardy@gmail.com', '1199257521'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaProfesor]'Samuel', 'L.Jackson', 'samuel.l.jackson@gmail.com', '1128076725'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaProfesor]'Sandra', 'Bullock', 'sandra.bullock@gmail.com', '1117627925'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaProfesor]'Nicole', 'Kidman', 'nicole.kidman@gmail.com', '1160507433'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaProfesor]'Cameron', 'Diaz', 'cameron.diaz@gmail.com', '1120021036'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+DECLARE @apellido varchar(100)
+DECLARE @email varchar(255)
+DECLARE @telefono varchar(255)
+
+EXECUTE @RC = [dbo].[altaProfesor]'Gerard', 'Butler', 'gerard.butler@gmail.com', '1162305533'
+GO
