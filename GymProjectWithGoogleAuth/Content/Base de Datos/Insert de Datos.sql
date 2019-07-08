421e49a0d65ce665508c96aec8279be6de72e071
Tomás Vainstein <tomasvainstein@gmail.com>
2019-07-08 02:44:18 -0300

    Actualización del 'Insert de Datos'

--- GymProjectWithGoogleAuth/Content/Base de Datos/Insert de Datos.sql
+++ GymProjectWithGoogleAuth/Content/Base de Datos/Insert de Datos.sql
@@ -7,7 +7,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Alumno', 'HomersGym', 'alumno.homersgym@gmail.com', '1196587989'
+EXECUTE @RC = [dbo].[altaAlumno]'Morgan', 'Freeman', 'morgan.freeman@gmail.com', '11-7081-0538'
 GO
 
 DECLARE @RC int
@@ -16,7 +16,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Morgan', 'Freeman', 'morgan.freeman@gmail.com', '1170810538'
+EXECUTE @RC = [dbo].[altaAlumno]'Johnny', 'Deep', 'johnny.deep@gmail.com', '11-6054-8582'
 GO
 
 DECLARE @RC int
@@ -25,7 +25,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Johnny', 'Deep', 'johnny.deep@gmail.com', '1160548582'
+EXECUTE @RC = [dbo].[altaAlumno]'Al', 'Pacino', 'al.pacino@gmail.com', '11-5004-2473'
 GO
 
 DECLARE @RC int
@@ -34,7 +34,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Al', 'Pacino', 'al.pacino@gmail.com', '1150042473'
+EXECUTE @RC = [dbo].[altaAlumno]'Robert', 'De Niro', 'robert.deniro@gmail.com', '11-4596-0050'
 GO
 
 DECLARE @RC int
@@ -43,7 +43,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Robert', 'DeNiro', 'robert.deniro@gmail.com', '1145960050'
+EXECUTE @RC = [dbo].[altaAlumno]'Denzel', 'Washington', 'denzel.washington@gmail.com', '11-8993-0009'
 GO
 
 DECLARE @RC int
@@ -52,7 +52,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Denzel', 'Washington', 'denzel.washington@gmail.com', '1189930009'
+EXECUTE @RC = [dbo].[altaAlumno]'Brad', 'Pitt', 'brad.pitt@gmail.com', '11-5920-0265'
 GO
 
 DECLARE @RC int
@@ -61,7 +61,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Brad', 'Pitt', 'brad.pitt@gmail.com', '1159200265'
+EXECUTE @RC = [dbo].[altaAlumno]'Bruce', 'Lee', 'bruce.lee@gmail.com', '11-2988-7183'
 GO
 
 DECLARE @RC int
@@ -70,7 +70,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Bruce', 'Lee', 'bruce.lee@gmail.com', '1129887183'
+EXECUTE @RC = [dbo].[altaAlumno]'Angelina', 'Jolie', 'angelina.jolie@gmail.com', '11-7099-2449'
 GO
 
 DECLARE @RC int
@@ -79,7 +79,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Angelina', 'Jolie', 'angelina.jolie@gmail.com', '1170992449'
+EXECUTE @RC = [dbo].[altaAlumno]'Leonardo', 'DiCaprio', 'leonardo.dicaprio@gmail.com', '11-6557-0252'
 GO
 
 DECLARE @RC int
@@ -88,7 +88,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Leonardo', 'DiCaprio', 'leonardo.dicaprio@gmail.com', '1165570252'
+EXECUTE @RC = [dbo].[altaAlumno]'Tom', 'Cruise', 'tom.cruise@gmail.com', '11-3150-7294'
 GO
 
 DECLARE @RC int
@@ -97,7 +97,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Tom', 'Cruise', 'tom.cruise@gmail.com', '1131507294'
+EXECUTE @RC = [dbo].[altaAlumno]'John', 'Travolta', 'john.travolta@gmail.com', '11-3438-6487'
 GO
 
 DECLARE @RC int
@@ -106,7 +106,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'John', 'Travolta', 'john.travolta@gmail.com', '1134386487'
+EXECUTE @RC = [dbo].[altaAlumno]'Arnold', 'Schwarzenegger', 'arnold.schwarzenegger@gmail.com', '11-6021-7987'
 GO
 
 DECLARE @RC int
@@ -115,7 +115,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Arnold', 'Schwarzenegger', 'arnold.schwarzenegger@gmail.com', '1160217987'
+EXECUTE @RC = [dbo].[altaAlumno]'Sylvester', 'Stallone', 'sylvester.stallone@gmail.com', '11-7086-9700'
 GO
 
 DECLARE @RC int
@@ -124,7 +124,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Sylvester', 'Stallone', 'sylvester.stallone@gmail.com', '1170869700'
+EXECUTE @RC = [dbo].[altaAlumno]'Christian', 'Bale', 'christian.bale@gmail.com', '11-7008-5187'
 GO
 
 DECLARE @RC int
@@ -133,7 +133,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Christian', 'Bale', 'christian.bale@gmail.com', '1170085187'
+EXECUTE @RC = [dbo].[altaAlumno]'Keanu', 'Reeves', 'keanu.reeves@gmail.com', '11-3215-5732'
 GO
 
 DECLARE @RC int
@@ -142,7 +142,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Keanu', 'Reeves', 'keanu.reeves@gmail.com', '1132155732'
+EXECUTE @RC = [dbo].[altaAlumno]'Nicolas', 'Cage', 'nicolas.cage@gmail.com', '11-5016-0998'
 GO
 
 DECLARE @RC int
@@ -151,7 +151,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Nicolas', 'Cage', 'nicolas.cage@gmail.com', '1150160998'
+EXECUTE @RC = [dbo].[altaAlumno]'Hugh', 'Jackman', 'hugh.jackman@gmail.com', '11-1128-7923'
 GO
 
 DECLARE @RC int
@@ -160,7 +160,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Hugh', 'Jackman', 'hugh.jackman@gmail.com', '1111287923'
+EXECUTE @RC = [dbo].[altaAlumno]'Edward', 'Norton', 'edward.norton@gmail.com', '11-2077-5567'
 GO
 
 DECLARE @RC int
@@ -169,7 +169,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Edward', 'Norton', 'edward.norton@gmail.com', '1120775567'
+EXECUTE @RC = [dbo].[altaAlumno]'Bruce', 'Willis', 'bruce.willis@gmail.com', '11-2911-0092'
 GO
 
 DECLARE @RC int
@@ -178,7 +178,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Bruce', 'Willis', 'bruce.willis@gmail.com', '1129110092'
+EXECUTE @RC = [dbo].[altaAlumno]'Tom', 'Hanks', 'tom.hanks@gmail.com', '11-3807-7619'
 GO
 
 DECLARE @RC int
@@ -187,7 +187,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Tom', 'Hanks', 'tom.hanks@gmail.com', '1138077619'
+EXECUTE @RC = [dbo].[altaAlumno]'Find Way Out', 'Soft', 'findwayoutsoft@gmail.com', '11-7081-0538'
 GO
 
 DECLARE @RC int
@@ -196,7 +196,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaAlumno]'Find Way Out', 'Soft', 'findwayoutsoft@gmail.com', '1170810538'
+EXECUTE @RC = [dbo].[altaAlumno]'Alumno', 'HomersGym', 'alumno.homersgym@gmail.com', '11-9658-7989'
 GO
 
 DECLARE @RC int
@@ -205,7 +205,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaProfesor]'Profesor', 'HomersGym', 'profesor.homersgym@gmail.com', '1193396555'
+EXECUTE @RC = [dbo].[altaProfesor]'Will', 'Smith', 'will.smith@gmail.com', '11-9339-6555'
 GO
 
 DECLARE @RC int
@@ -214,7 +214,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaProfesor]'Will', 'Smith', 'will.smith@gmail.com', '1193396555'
+EXECUTE @RC = [dbo].[altaProfesor]'Vin', 'Diesel', 'vin.diesel@gmail.com', '11-1558-2015'
 GO
 
 DECLARE @RC int
@@ -223,7 +223,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaProfesor]'Vin', 'Diesel', 'vin.diesel@gmail.com', '1115582015'
+EXECUTE @RC = [dbo].[altaProfesor]'Matt', 'Damon', 'matt.damon@gmail.com', '11-2530-7492'
 GO
 
 DECLARE @RC int
@@ -232,7 +232,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaProfesor]'Matt', 'Damon', 'matt.damon@gmail.com', '1125307492'
+EXECUTE @RC = [dbo].[altaProfesor]'Tom', 'Hardy', 'tom.hardy@gmail.com', '11-9925-7521'
 GO
 
 DECLARE @RC int
@@ -241,7 +241,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaProfesor]'Tom', 'Hardy', 'tom.hardy@gmail.com', '1199257521'
+EXECUTE @RC = [dbo].[altaProfesor]'Samuel', 'Jackson', 'samuel.jackson@gmail.com', '11-2807-6725'
 GO
 
 DECLARE @RC int
@@ -250,7 +250,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaProfesor]'Samuel', 'L.Jackson', 'samuel.l.jackson@gmail.com', '1128076725'
+EXECUTE @RC = [dbo].[altaProfesor]'Sandra', 'Bullock', 'sandra.bullock@gmail.com', '11-1762-7925'
 GO
 
 DECLARE @RC int
@@ -259,7 +259,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaProfesor]'Sandra', 'Bullock', 'sandra.bullock@gmail.com', '1117627925'
+EXECUTE @RC = [dbo].[altaProfesor]'Nicole', 'Kidman', 'nicole.kidman@gmail.com', '11-6050-7433'
 GO
 
 DECLARE @RC int
@@ -268,7 +268,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaProfesor]'Nicole', 'Kidman', 'nicole.kidman@gmail.com', '1160507433'
+EXECUTE @RC = [dbo].[altaProfesor]'Cameron', 'Diaz', 'cameron.diaz@gmail.com', '11-2002-1036'
 GO
 
 DECLARE @RC int
@@ -277,7 +277,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaProfesor]'Cameron', 'Diaz', 'cameron.diaz@gmail.com', '1120021036'
+EXECUTE @RC = [dbo].[altaProfesor]'Gerard', 'Butler', 'gerard.butler@gmail.com', '11-6230-5533'
 GO
 
 DECLARE @RC int
@@ -286,7 +286,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaProfesor]'Gerard', 'Butler', 'gerard.butler@gmail.com', '1162305533'
+EXECUTE @RC = [dbo].[altaProfesor]'Federico', 'Martín', 'femartin73@gmail.com', '11-3478-0064'
 GO
 
 DECLARE @RC int
@@ -295,7 +295,7 @@ DECLARE @apellido varchar(100)
 DECLARE @email varchar(255)
 DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaProfesor]'Federico', 'Martín', 'femartin73@gmail.com', '1162305533'
+EXECUTE @RC = [dbo].[altaProfesor]'Profesor', 'HomersGym', 'profesor.homersgym@gmail.com', '11-9368-2890'
 GO
 
 DECLARE @RC int
@@ -317,55 +317,11 @@ EXECUTE @RC = [dbo].[altaAdministrador]'aprosoff.federico@gmail.com'
 GO
 
 DECLARE @RC int
-DECLARE @nombre varchar(100)
-
-EXECUTE @RC = [dbo].[altaActividad]'Natación'
-GO
-
-DECLARE @RC int
-DECLARE @nombre varchar(100)
-
-EXECUTE @RC = [dbo].[altaActividad]'Musculación'
-GO
-
-DECLARE @RC int
-DECLARE @nombre varchar(100)
-
-EXECUTE @RC = [dbo].[altaActividad]'Spinning'
-GO
-
-DECLARE @RC int
-DECLARE @nombre varchar(100)
-
-EXECUTE @RC = [dbo].[altaActividad]'Cardio'
-GO
-
-DECLARE @RC int
-DECLARE @nombre varchar(100)
-
-EXECUTE @RC = [dbo].[altaActividad]'Yoga'
-GO
-
-DECLARE @RC int
-DECLARE @nombre varchar(100)
-
-EXECUTE @RC = [dbo].[altaActividad]'Taekwondo'
-GO
-
-DECLARE @RC int
 DECLARE @barrio varchar(100)
 DECLARE @direccion varchar(100)
 DECLARE @telefono varchar(255)
 
 EXECUTE @RC = [dbo].[altaSucursal]'Almagro','Yatay 240','4020-4321'
-GO
-
-DECLARE @RC int
-DECLARE @barrio varchar(100)
-DECLARE @direccion varchar(100)
-DECLARE @telefono varchar(255)
-
-EXECUTE @RC = [dbo].[altaSucursal]'Saavedra','Vedia 3600','5777-9971'
 GO
 
 DECLARE @RC int
@@ -393,12 +349,71 @@ EXECUTE @RC = [dbo].[altaSucursal]'Palermo','Costa Rica 4677','6739-0110'
 GO
 
 DECLARE @RC int
-DECLARE	@nroSucursal int
-DECLARE	@cantCreditos int
-DECLARE	@diasVigencia int
-DECLARE	@precio float
+DECLARE @barrio varchar(100)
+DECLARE @direccion varchar(100)
+DECLARE @telefono varchar(255)
 
-EXECUTE @RC = [dbo].[altaPack] 1, 10, 30, 100
+EXECUTE @RC = [dbo].[altaSucursal]'Saavedra','Vedia 3600','5777-9971'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+
+EXECUTE @RC = [dbo].[altaActividad]'Abdominales'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+
+EXECUTE @RC = [dbo].[altaActividad]'Aero Local'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+
+EXECUTE @RC = [dbo].[altaActividad]'Cardio'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+
+EXECUTE @RC = [dbo].[altaActividad]'Functional Training'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+
+EXECUTE @RC = [dbo].[altaActividad]'Musculación'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+
+EXECUTE @RC = [dbo].[altaActividad]'Natación'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+
+EXECUTE @RC = [dbo].[altaActividad]'Spinning'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+
+EXECUTE @RC = [dbo].[altaActividad]'Taekwondo'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+
+EXECUTE @RC = [dbo].[altaActividad]'Yoga'
+GO
+
+DECLARE @RC int
+DECLARE @nombre varchar(100)
+
+EXECUTE @RC = [dbo].[altaActividad]'Zumba'
 GO
 
 DECLARE @RC int
@@ -407,7 +422,7 @@ DECLARE	@cantCreditos int
 DECLARE	@diasVigencia int
 DECLARE	@precio float
 
-EXECUTE @RC = [dbo].[altaPack] 1, 50, 120, 400
+EXECUTE @RC = [dbo].[altaPack] 1, 10, 15, 100
 GO
 
 DECLARE @RC int
@@ -416,7 +431,7 @@ DECLARE	@cantCreditos int
 DECLARE	@diasVigencia int
 DECLARE	@precio float
 
-EXECUTE @RC = [dbo].[altaPack] 2, 10, 30, 120
+EXECUTE @RC = [dbo].[altaPack] 1, 25, 40, 225
 GO
 
 DECLARE @RC int
@@ -425,7 +440,7 @@ DECLARE	@cantCreditos int
 DECLARE	@diasVigencia int
 DECLARE	@precio float
 
-EXECUTE @RC = [dbo].[altaPack] 2, 50, 120, 500
+EXECUTE @RC = [dbo].[altaPack] 1, 50, 75, 375
 GO
 
 DECLARE @RC int
@@ -434,7 +449,7 @@ DECLARE	@cantCreditos int
 DECLARE	@diasVigencia int
 DECLARE	@precio float
 
-EXECUTE @RC = [dbo].[altaPack] 3, 10, 30, 75.50
+EXECUTE @RC = [dbo].[altaPack] 1, 100, 150, 700
 GO
 
 DECLARE @RC int
@@ -443,7 +458,142 @@ DECLARE	@cantCreditos int
 DECLARE	@diasVigencia int
 DECLARE	@precio float
 
-EXECUTE @RC = [dbo].[altaPack] 3, 50, 120, 300
+EXECUTE @RC = [dbo].[altaPack] 2, 10, 15, 110
+GO
+
+DECLARE @RC int
+DECLARE	@nroSucursal int
+DECLARE	@cantCreditos int
+DECLARE	@diasVigencia int
+DECLARE	@precio float
+
+EXECUTE @RC = [dbo].[altaPack] 2, 25, 40, 247,50
+GO
+
+DECLARE @RC int
+DECLARE	@nroSucursal int
+DECLARE	@cantCreditos int
+DECLARE	@diasVigencia int
+DECLARE	@precio float
+
+EXECUTE @RC = [dbo].[altaPack] 2, 50, 75, 412,50
+GO
+
+DECLARE @RC int
+DECLARE	@nroSucursal int
+DECLARE	@cantCreditos int
+DECLARE	@diasVigencia int
+DECLARE	@precio float
+
+EXECUTE @RC = [dbo].[altaPack] 2, 100, 150, 770
+GO
+
+DECLARE @RC int
+DECLARE	@nroSucursal int
+DECLARE	@cantCreditos int
+DECLARE	@diasVigencia int
+DECLARE	@precio float
+
+EXECUTE @RC = [dbo].[altaPack] 3, 10, 15, 130
+GO
+
+DECLARE @RC int
+DECLARE	@nroSucursal int
+DECLARE	@cantCreditos int
+DECLARE	@diasVigencia int
+DECLARE	@precio float
+
+EXECUTE @RC = [dbo].[altaPack] 3, 25, 40, 292,50
+GO
+
+DECLARE @RC int
+DECLARE	@nroSucursal int
+DECLARE	@cantCreditos int
+DECLARE	@diasVigencia int
+DECLARE	@precio float
+
+EXECUTE @RC = [dbo].[altaPack] 3, 50, 75, 487,50
+GO
+
+DECLARE @RC int
+DECLARE	@nroSucursal int
+DECLARE	@cantCreditos int
+DECLARE	@diasVigencia int
+DECLARE	@precio float
+
+EXECUTE @RC = [dbo].[altaPack] 3, 100, 150, 910
+GO
+
+DECLARE @RC int
+DECLARE	@nroSucursal int
+DECLARE	@cantCreditos int
+DECLARE	@diasVigencia int
+DECLARE	@precio float
+
+EXECUTE @RC = [dbo].[altaPack] 4, 10, 15, 150
+GO
+
+DECLARE @RC int
+DECLARE	@nroSucursal int
+DECLARE	@cantCreditos int
+DECLARE	@diasVigencia int
+DECLARE	@precio float
+
+EXECUTE @RC = [dbo].[altaPack] 4, 25, 40, 337,50
+GO
+
+DECLARE @RC int
+DECLARE	@nroSucursal int
+DECLARE	@cantCreditos int
+DECLARE	@diasVigencia int
+DECLARE	@precio float
+
+EXECUTE @RC = [dbo].[altaPack] 4, 50, 75, 562,50
+GO
+
+DECLARE @RC int
+DECLARE	@nroSucursal int
+DECLARE	@cantCreditos int
+DECLARE	@diasVigencia int
+DECLARE	@precio float
+
+EXECUTE @RC = [dbo].[altaPack] 4, 100, 150, 1050
+GO
+
+DECLARE @RC int
+DECLARE	@nroSucursal int
+DECLARE	@cantCreditos int
+DECLARE	@diasVigencia int
+DECLARE	@precio float
+
+EXECUTE @RC = [dbo].[altaPack] 5, 10, 15, 120
+GO
+
+DECLARE @RC int
+DECLARE	@nroSucursal int
+DECLARE	@cantCreditos int
+DECLARE	@diasVigencia int
+DECLARE	@precio float
+
+EXECUTE @RC = [dbo].[altaPack] 5, 25, 40, 270
+GO
+
+DECLARE @RC int
+DECLARE	@nroSucursal int
+DECLARE	@cantCreditos int
+DECLARE	@diasVigencia int
+DECLARE	@precio float
+
+EXECUTE @RC = [dbo].[altaPack] 5, 50, 75, 450
+GO
+
+DECLARE @RC int
+DECLARE	@nroSucursal int
+DECLARE	@cantCreditos int
+DECLARE	@diasVigencia int
+DECLARE	@precio float
+
+EXECUTE @RC = [dbo].[altaPack] 5, 100, 150, 840
 GO
 
 DECLARE @RC int
